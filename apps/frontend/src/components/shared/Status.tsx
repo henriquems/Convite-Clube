7410ff77b90340a00b9dfdda27256beb2e627d84
Henrique Simoes <henriquems@gmail.com>
2025-05-20 15:54:29 -0300

    modificação componente status

--- apps/frontend/src/components/shared/Status.tsx
+++ apps/frontend/src/components/shared/Status.tsx
@@ -6,7 +6,7 @@ export default function Status(props: StatusProps) {
     return (
         <div className={`flex items-center justify-center
             rounded-lg p-1 text-white text-xs w-[65px]
-            ${props.valor === 'ATIVO' ? 'bg-green-700' : 'bg-red-600'}
+            ${props.valor === 'ATIVO' ? 'bg-green-700 hover:bg-green-600' : 'bg-red-600 hover:bg-red-500'}
         `}>
             {props.valor}
         </div>
